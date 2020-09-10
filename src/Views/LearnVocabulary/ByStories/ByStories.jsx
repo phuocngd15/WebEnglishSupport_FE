34b05b5dec8c7ebcea45b2bd7c63438017915e3b
phuocngd15 <phuocngd15@gmail.com>
2020-09-10 21:38:04 +0700

    alittle

---
+++ src/Views/LearnVocabulary/ByStories/ByStories.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState, useRef } from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios';
+import { TestcallApiAzue } from './service/serverApi';
+
+
+const Player = ({ url }) => {
+    const [playing, toggle] = useAudio(url);
+
+    return (
+        <div>
+            <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+        </div>
+    );
+};
+
+const ByStories = ({ isStory }) => {
+    return (
+        <Player url={'https://crudapitest.azurewebsites.net/tracks/5f54af4587e40f62f8ede087'} />
+    );
+}
+ByStories.propTypes = {
+    prop: PropTypes
+}
+export default ByStories
---
+++ src/Views/LearnVocabulary/ByStories/service/getStories.jsx
@@ -0,0 +1,11 @@
+// nơi gọi axio và xử lí
+
+import React, { useEffect } from 'react'
+// trang admin : create/ update/delete
+// trang client: readt
+
+const Story = () => {
+    return (
+        <div>story</div>
+    )
+}
