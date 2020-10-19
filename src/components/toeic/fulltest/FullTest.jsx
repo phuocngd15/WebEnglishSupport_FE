e9d9b5409a178b377e0b435f902b2865d3ae2d93
NguyenTranThanhTruc <55777063+NguyenTranThanhTruc@users.noreply.github.com>
2020-10-19 23:36:46 +0700

    carousel layout

--- src/components/toeic/fulltest/FullTest.jsx
+++ src/components/toeic/fulltest/FullTest.jsx
@@ -1,66 +1,127 @@
-import React from 'react';
+import React from 'react'
 import {
-    CCard,
-    CCardBody,
-    CCardHeader,
-    CCol,
-    CRow,
-    CContainer,
-    CCarousel,
-    CCarouselCaption,
-    CCarouselControl,
-    CCarouselIndicators,
-    CCarouselInner,
-    CCarouselItem,
-} from '@coreui/react';
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CContainer,
+  CCarousel,
+  CCarouselCaption,
+  CCarouselControl,
+  CCarouselIndicators,
+  CCarouselInner,
+  CCarouselItem
+} from '@coreui/react'
 
-import routes from '../../../containers/routes';
-
-const slides = [
-    'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1607923e7e2%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1607923e7e2%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9296875%22%20y%3D%22217.75625%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-]
+import routes from '../../../containers/routes'
+import '../../../scss/style.scss'
+const slides = [1, 2, 3, 4, 5, 6]
 
 const FullTest = () => {
-    return (
-        <CContainer className="containertoeic">
-            <CRow className="row rowtoeic-text">
-                <CContainer className="testfullwrap">
-                    <CCol lg="5" className="testfullwrapinner testfullwrap_one">
-                        <span className="testfullonetwo testfull-one">TEST FULL</span>
-                        <span className="testfullonetwo testfull-two">LISTENING + READING</span>
-                    </CCol>
-                    <CCol lg="5" className="testfullwrapinner testfullwrap-two" >
-                        <span className="testfullonetwo testfull-one">KIỂM TRA ĐẦY ĐỦ</span>
-                        <span className="testfullonetwo testfull-two">NGHE + ĐỌC</span>
-                    </CCol>
-                </CContainer>
-            </CRow>
-            <CCol xs="12" xl="6" className="rowtoeic-exam">
-                <CCard>
-                    <CCarousel animate autoSlide={2000}>
-                        <CCarouselInner>
-                            <CCarouselItem>
-                                <img className="d-block w-100" src={slides[0]} alt="slide 1" />
-                                <CCarouselCaption><h3>Slide 1</h3><p>Slide 1</p></CCarouselCaption>
-                            </CCarouselItem>
-                            <CCarouselItem>
-                                <img className="d-block w-100" src={slides[1]} alt="slide 2" />
-                                <CCarouselCaption><h3>Slide 2</h3><p>Slide 2</p></CCarouselCaption>
-                            </CCarouselItem>
-                            <CCarouselItem>
-                                <img className="d-block w-100" src={slides[2]} alt="slide 3" />
-                                <CCarouselCaption><h3>Slide 3</h3><p>Slide 3</p></CCarouselCaption>
-                            </CCarouselItem>
-                        </CCarouselInner>
-                        <CCarouselControl direction="prev" />
-                        <CCarouselControl direction="next" />
-                    </CCarousel>
-                </CCard>
-            </CCol>
+  return (
+    <CContainer className="containertoeic">
+      <CRow className="row rowtoeic-text">
+        <CContainer className="testfullwrap">
+          <CCol lg="5" className="testfullwrapinner testfullwrap_one">
+            <span className="testfullonetwo testfull-one">TEST FULL</span>
+            <span className="testfullonetwo testfull-two">
+              LISTENING + READING
+            </span>
+          </CCol>
+          <CCol lg="5" className="testfullwrapinner testfullwrap-two">
+            <span className="testfullonetwo testfull-one">KIỂM TRA ĐẦY ĐỦ</span>
+            <span className="testfullonetwo testfull-two">NGHE + ĐỌC</span>
+          </CCol>
         </CContainer>
-    )
+      </CRow>
+      <CRow>
+        <CCarousel animate autoSlide={2000}>
+          <CCarouselInner>
+            <CRow>
+              <CCarouselItem>
+                <CRow className="inner-carousel">
+                  <CCol md="3" className="rowtoeic-exam m-2 py-3">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[0]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                  <CCol md="3" className="rowtoeic-exam m-2 py-3">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[0]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                  <CCol md="3" className="rowtoeic-exam m-2 py-3">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[0]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                </CRow>
+              </CCarouselItem>
+              <CCarouselItem>
+                <CRow className="inner-carousel">
+                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[1]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[1]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[1]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                </CRow>
+              </CCarouselItem>
+              <CCarouselItem>
+                <CRow className="inner-carousel">
+                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[2]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[2]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                    <CCard borderColor="info">
+                      <h3>Đề thi {slides[2]}</h3>
+                      <h3>Thời gian: 02h 00m</h3>
+                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                    </CCard>
+                  </CCol>
+                </CRow>
+              </CCarouselItem>
+            </CRow>
+          </CCarouselInner>
+          <CCarouselControl direction="prev" />
+          <CCarouselControl direction="next" />
+        </CCarousel>
+      </CRow>
+    </CContainer>
+  )
 }
 
 export default FullTest
