d930d453596c5609489267bba744d6399edc1c38
NguyenTranThanhTruc <55777063+NguyenTranThanhTruc@users.noreply.github.com>
2020-10-22 21:02:30 +0700

    update card fe

--- src/components/toeic/fulltest/FullTest.jsx
+++ src/components/toeic/fulltest/FullTest.jsx
@@ -20,106 +20,128 @@ const slides = [1, 2, 3, 4, 5, 6]
 
 const FullTest = () => {
   return (
-    <CContainer className="containertoeic">
-      <CRow className="row rowtoeic-text">
-        <CContainer className="testfullwrap">
-          <CCol lg="5" className="testfullwrapinner testfullwrap_one">
-            <span className="testfullonetwo testfull-one">TEST FULL</span>
-            <span className="testfullonetwo testfull-two">
-              LISTENING + READING
-            </span>
-          </CCol>
-          <CCol lg="5" className="testfullwrapinner testfullwrap-two">
-            <span className="testfullonetwo testfull-one">KIỂM TRA ĐẦY ĐỦ</span>
-            <span className="testfullonetwo testfull-two">NGHE + ĐỌC</span>
-          </CCol>
-        </CContainer>
-      </CRow>
-      <CRow>
-        <CCarousel animate autoSlide={2000}>
-          <CCarouselInner>
-            <CRow>
-              <CCarouselItem>
-                <CRow className="inner-carousel">
-                  <CCol md="3" className="rowtoeic-exam m-2 py-3">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[0]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                  <CCol md="3" className="rowtoeic-exam m-2 py-3">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[0]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                  <CCol md="3" className="rowtoeic-exam m-2 py-3">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[0]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                </CRow>
-              </CCarouselItem>
-              <CCarouselItem>
-                <CRow className="inner-carousel">
-                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[1]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[1]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[1]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                </CRow>
-              </CCarouselItem>
-              <CCarouselItem>
-                <CRow className="inner-carousel">
-                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[2]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[2]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                  <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
-                    <CCard borderColor="info">
-                      <h3>Đề thi {slides[2]}</h3>
-                      <h3>Thời gian: 02h 00m</h3>
-                      <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
-                    </CCard>
-                  </CCol>
-                </CRow>
-              </CCarouselItem>
-            </CRow>
-          </CCarouselInner>
-          <CCarouselControl direction="prev" />
-          <CCarouselControl direction="next" />
-        </CCarousel>
-      </CRow>
+    <CContainer>
+      <CContainer className="containertoeic">
+        <CRow className="row rowtoeic-text m-2">
+          <CContainer className="testfullwrap">
+            <CCol lg="5" className="testfullwrapinner testfullwrap_one">
+              <span className="testfullonetwo testfull-one">TEST FULL</span>
+              <span className="testfullonetwo testfull-two">
+                LISTENING + READING
+              </span>
+            </CCol>
+            <CCol lg="5" className="testfullwrapinner testfullwrap-two">
+              <span className="testfullonetwo testfull-one">
+                KIỂM TRA ĐẦY ĐỦ
+              </span>
+              <span className="testfullonetwo testfull-two">NGHE + ĐỌC</span>
+            </CCol>
+          </CContainer>
+        </CRow>
+        <CRow className="row rowtoeic-exam">
+          <CCarousel animate autoSlide={2000}>
+            <CCarouselInner>
+              <CRow>
+                <CCarouselItem>
+                  <CRow className="inner-carousel">
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[0]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[0]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[0]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                  </CRow>
+                </CCarouselItem>
+                <CCarouselItem>
+                  <CRow className="inner-carousel">
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[1]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[1]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[1]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                  </CRow>
+                </CCarouselItem>
+                <CCarouselItem>
+                  <CRow className="inner-carousel">
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[2]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[2]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                    <CCol md="3" className="rowtoeic-exam bg-success m-2 py-5">
+                      <CCard borderColor="info">
+                        <h3>Đề thi {slides[2]}</h3>
+                        <h3>Thời gian: 02h 00m</h3>
+                        <h4>Đề thi TOEIC IIG chuẩn ngày 08/10/2020</h4>
+                      </CCard>
+                    </CCol>
+                  </CRow>
+                </CCarouselItem>
+              </CRow>
+            </CCarouselInner>
+            <CCarouselControl direction="prev" />
+            <CCarouselControl direction="next" />
+          </CCarousel>
+        </CRow>
+      </CContainer>
+      <CContainer className>
+        <CRow className="row rowtoeic-text m-3">
+          <CContainer className="testfullwrap">
+            <CCol lg="5" className="testfullwrapinner testfullwrap_one">
+              <span className="testfullonetwo testfull-one">TEST FULL</span>
+              <span className="testfullonetwo testfull-two">
+                LISTENING + READING
+              </span>
+            </CCol>
+            <CCol lg="5" className="testfullwrapinner testfullwrap-two">
+              <span className="testfullonetwo testfull-one">
+                KIỂM TRA ĐẦY ĐỦ
+              </span>
+              <span className="testfullonetwo testfull-two">NGHE + ĐỌC</span>
+            </CCol>
+          </CContainer>
+        </CRow>
+      </CContainer>
     </CContainer>
   )
 }
