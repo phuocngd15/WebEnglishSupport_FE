35237298e830c415d44ec7ecfe3ba54f7cb4e8bf
phuocngd15 <phuocngd15@gmail.com>
2020-12-26 18:13:09 +0700

    clean code

--- src/components/settings/profile/Profile.jsx
+++ src/components/settings/profile/Profile.jsx
@@ -1,34 +1,11 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { validate } from 'email-validator';
-import useEncrypt from '../../hook/useEncrypt';
-import { axiosPost } from '../../../axios/axios';
-import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CCol,
-  CForm,
-  CFormGroup,
-  CLabel,
-  CContainer,
-  CLink,
-  CListGroup,
-  CListGroupItem,
-  CRow
-} from '@coreui/react';
-import { mdiAccountEditOutline } from '@mdi/js';
-import Icon from '@mdi/react';
-import CIcon from '@coreui/icons-react';
-import { Link } from 'react-router-dom';
-import Axios from 'axios';
-import { getValueRef } from '../../../share/func';
-import { StatusMiddleWare } from '../../../share/Alert';
+import React from 'react';
+import { CCol, CListGroup, CListGroupItem, CRow } from '@coreui/react';
+import { OneInput } from './Component/CoreComponent/OneInput';
+
 const GeneralEdit = () => {
   return (
     <>
-      <OneItemCanEdit
+      <OneInput
         keyQuery='fullname'
         label='Họ và tên'
         placeholder='nguyen van a'
@@ -36,7 +13,7 @@ const GeneralEdit = () => {
         apiPostURL={'http://localhost:9999/api/profile/name'}
         apiGetURL={'http://localhost:9999/api/profile'}
       />
-      <OneItemCanEdit
+      <OneInput
         label='SĐT'
         keyQuery='phone'
         placeholder='0123456789'
@@ -48,113 +25,7 @@ const GeneralEdit = () => {
   );
 };
 const Password = () => {
-  return (
-    <OneItemCanEdit label='Mật khẩu' placeholder='*******' type='password' />
-  );
-};
-
-const OneItemCanEdit = props => {
-  const email = useSelector(state => state.authentication.loginState.email);
-  const {
-    label,
-    placeholder,
-    type,
-    apiPostURL,
-    apiGetURL,
-    needEncrypt = false,
-    keyQuery
-  } = props;
-  const dispatch = useDispatch();
-  const [isViewMode, setIsViewMode] = useState(true);
-  const [mahoa] = useEncrypt();
-  const [value, setValue] = useState(placeholder);
-
-  const inputRef = useRef();
-
-  const handleSave = async () => {
-    const model = {
-      email: email,
-      value: needEncrypt ? mahoa(getValueRef(inputRef)) : getValueRef(inputRef)
-    };
-    const res = await Axios.post(apiPostURL, model);
-    StatusMiddleWare(res.status, res.data) && setIsViewMode(true);
-  };
-  useEffect(() => {
-    let cancelled = false;
-    (async () => {
-      const response = await Axios.get(apiGetURL, {
-        params: {
-          email
-        }
-      });
-      if (!cancelled) {
-        const { data } = response;
-        setValue(data[keyQuery]);
-      }
-    })();
-    return () => {
-      cancelled = true;
-    };
-  }, [apiGetURL, email, keyQuery, placeholder]);
-  const handleChange = e => {
-    setValue();
-  };
-  const renderBtn = isShow => {
-    if (isShow)
-      return (
-        <>
-          <CButton
-            type='submit'
-            size='l'
-            className='item-btn-success'
-            color='primary'
-            variant='outline'
-            onClick={handleSave}>
-            Lưu
-          </CButton>
-          <CButton
-            type='submit'
-            size='l'
-            variant='outline'
-            className='item-btn-cancel'
-            color='dark'
-            onClick={() => setIsViewMode(true)}>
-            Hủy
-          </CButton>
-        </>
-      );
-    return null;
-  };
-  return (
-    <CRow className='item'>
-      <CCol xs='12' md='2'>
-        <CLabel className='item-label'>{label}</CLabel>
-      </CCol>
-      <CCol xs='12' md='10'>
-        <input
-          ref={inputRef}
-          type={type}
-          //  placeholder={placeholder}
-          value={value}
-          disabled={isViewMode}
-          onChange={e => handleChange(e.target.value)}
-        />
-        <span className='item-control'>
-          {renderBtn(!isViewMode)}
-          {isViewMode && (
-            <span
-              className='item-btnEdit'
-              size='l'
-              color='primary'
-              onClick={() => setIsViewMode(false)}
-              variant='outline'>
-              Chỉnh sửa
-            </span>
-          )}
-        </span>
-      </CCol>
-    </CRow>
-  );
+  return <OneInput label='Mật khẩu' placeholder='*******' type='password' />;
 };
 
 const ConfigList = () => {
