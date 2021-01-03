56dd84c1ed623ef03e6f0c2d1b4c39a265385117
NguyenTranThanhTruc <55777063+NguyenTranThanhTruc@users.noreply.github.com>
2021-01-03 15:13:09 +0700

    format

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { icons } from './assets/icons';
 import { Provider } from 'react-redux';
 import store from './Store/store';
 
-React.icons = icons;
 
 ReactDOM.render(
   <Provider store={store}>
